27ddcb342d3baf3923434cece37f7ef8f6ffdb65
Rob van Bentem <robvanbentem@gmail.com>
2017-07-06 19:21:06 +0200

    Added CORS handler

--- main.go
+++ main.go
@@ -9,6 +9,8 @@ import (
 	"net/http"
 	"fmt"
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/gorilla/handlers"
+	"os"
 )
 
 type Payload struct {
@@ -26,12 +28,21 @@ func main() {
 	defer gocmn.CloseDB()
 
 	r := mux.NewRouter()
+	loggedRouter := handlers.LoggingHandler(os.Stdout, r)
+	h := getCorsHandler(loggedRouter)
 	registerRoutes(r)
 
 	serve := fmt.Sprintf("%s:%d", common.ConfigRoot.Host, common.ConfigRoot.Port)
 
 	gocmn.Log.Infof("Server started on %s\n", serve)
-	gocmn.Log.Fatal(http.ListenAndServe(serve, r))
+	gocmn.Log.Fatal(http.ListenAndServe(serve, h))
+}
+
+func getCorsHandler(handler http.Handler) http.Handler {
+	headersOk := handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type"})
+	originsOk := handlers.AllowedOrigins([]string{"*"})
+	methodsOk := handlers.AllowedMethods([]string{"GET", "HEAD", "POST", "PUT", "DELETE", "OPTIONS"})
+	return handlers.CORS(originsOk, headersOk, methodsOk)(handler)
 }
 
 func registerRoutes(r *mux.Router) {
--- web/handler.go
+++ web/handler.go
@@ -4,27 +4,32 @@ import (
 	"net/http"
 	"github.com/robvanbentem/gocmn"
 	"encoding/json"
+	"time"
+	"strconv"
 )
 
 type Result struct {
-	Type     string
-	Device   string
-	Datetime string
-	Avg      float64
-	Min      float64
-	Max      float64
+	Type     string `json:"type"`
+	Device   string `json:"device"`
+	Datetime string `json:"date"`
+	Avg      float64 `json:"avg"`
+	Min      float64 `json:"min"`
+	Max      float64`json:"max"`
 }
 
 const QRY = "SELECT type, device, from_unixtime(avg(unix_timestamp(`date`))) as `datetime`, " +
-	"avg(value) as `avg`, min(value) as `min`, max(value) as `max` " +
-	"FROM data WHERE type = ? AND device = ? AND `date` > ? " +
-	"GROUP BY device, ROUND(UNIX_TIMESTAMP(`date`) / ?);"
+  "avg(value) as `avg`, min(value) as `min`, max(value) as `max` " +
+  "FROM data WHERE type = ? AND device = ? AND `date` > ? " +
+  "GROUP BY device, ROUND(UNIX_TIMESTAMP(`date`) / ?);"
 
 func Handle(w http.ResponseWriter, r *http.Request) {
 	typ := r.URL.Query().Get("type")
 	device := r.URL.Query().Get("device")
 	interval := r.URL.Query().Get("interval")
 	start := r.URL.Query().Get("start")
+
+	timestamp, _ := strconv.ParseInt(start, 10, 64)
+	start = time.Unix(timestamp, 0).Format("2006-01-02 15:04:05")
 
 	results := []Result{}
 
